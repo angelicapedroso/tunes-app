fe0f11c5438b83d4b2fce6bd9dccb713e2c75e9c
Angelica <angelicapedroso05@gmail.com>
2021-11-18 12:44:17 -0300

    requisito 2: cria campo de login e valida numero minimo de caracteres

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisabled: true,
+      inputLogin: '',
+    };
+    this.verificationButtonDisabled = this.verificationButtonDisabled.bind(this);
+    this.inputChange = this.inputChange.bind(this);
+  }
+
+  inputChange({ target }) {
+    const { value } = target;
+    this.setState({ inputLogin: value }, () => {
+      this.setState({ buttonDisabled: this.verificationButtonDisabled() });
+    });
+  }
+
+  verificationButtonDisabled() {
+    const { inputLogin } = this.state;
+    const minimumCharacterLogin = 3;
+    if (inputLogin.length >= minimumCharacterLogin) return false;
+    return true;
+  }
+
   render() {
+    const
+      {
+        buttonDisabled,
+        inputLogin,
+      } = this.state;
+
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="name">
+            Nome
+            <input
+              data-testid="login-name-input"
+              name="name"
+              type="text"
+              value={ inputLogin }
+              onChange={ this.inputChange }
+            />
+          </label>
+
+          <label htmlFor="button-login">
+            <input
+              data-testid="login-submit-button"
+              type="submit"
+              value="Entrar"
+              name="button-login"
+              // onClick={ createUser }
+              disabled={ buttonDisabled }
+            />
+          </label>
+        </form>
+      </div>
     );
   }
 }
