8fbc9366cafc1a1ecc709b7b268b348121a533d8
Angelica <angelicapedroso05@gmail.com>
2021-11-20 21:17:14 -0300

    requisito 3: usa a funcao getUser pra recuperar nome do usuario/ exibe o carregando... enquanto aguarda resposta

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import OnLoad from './OnLoad';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loginData: '',
+      loading: false,
+    };
+    this.retrieveUserData = this.retrieveUserData.bind(this);
+  }
+
+  componentDidMount() {
+    this.retrieveUserData();
+  }
+
+  async retrieveUserData() {
+    this.setState({ loading: true });
+    const { name } = await getUser();
+    this.setState({ loading: false, loginData: name });
+  }
+
   render() {
+    const { loginData, loading } = this.state;
     return (
-      <header data-testid="header-component"> </header>
-    );
+      loading ? (<OnLoad />) : (
+        <header data-testid="header-component">
+          <h2 data-testid="header-user-name">{ loginData }</h2>
+        </header>
+      ));
   }
 }
