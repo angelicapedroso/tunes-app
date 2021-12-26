e7f807af10be5b289cf15b6d4f846222f15c6f9d
Angelica <angelicapedroso05@gmail.com>
2021-12-26 15:07:32 -0300

    requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import OnLoad from '../components/OnLoad';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      name: '',
+      image: '',
+      email: '',
+      description: '',
+    };
+
+    this.restoreUserInfo = this.restoreUserInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.restoreUserInfo();
+  }
+
+  async restoreUserInfo() {
+    this.setState({ loading: true });
+    const data = await getUser();
+    this.setState({
+      loading: false,
+      name: data.name,
+      image: data.image,
+      email: data.email,
+      description: data.description,
+    });
+  }
+
   render() {
+    const { loading, name, image, email, description } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div>
         <Header />
+        <div data-testid="page-profile">
+          { loading
+            ? <OnLoad />
+            : (
+              <div>
+                <section>
+                  <img data-testid="profile-image" src={ image } alt={ name } />
+                  <Link to="/profile/edit">Editar perfil</Link>
+                  <p>
+                    Nome:
+                  </p>
+                  <p>
+                    { name }
+                  </p>
+                  <p>
+                    E-mail:
+                  </p>
+                  <p>
+                    { email }
+                  </p>
+                  <p>
+                    Descrição:
+                  </p>
+                  <p>
+                    { description }
+                  </p>
+                </section>
+              </div>
+            ) }
+        </div>
       </div>
     );
   }
