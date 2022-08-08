98bd220d905f17dad1aa8adaa694bfc1b29c6919
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-08 18:59:17 -0300

    refactor: remove onLoad component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { getUser } from '../services/userAPI';
-import OnLoad from '../components/OnLoad';
 
 export default class Profile extends Component {
   constructor() {
     super();
 
     this.state = {
-      loading: false,
       name: '',
       image: '',
       email: '',
@@ -24,10 +22,8 @@ export default class Profile extends Component {
   }
 
   async restoreUserInfo() {
-    this.setState({ loading: true });
     const data = await getUser();
     this.setState({
-      loading: false,
       name: data.name,
       image: data.image,
       email: data.email,
@@ -36,39 +32,23 @@ export default class Profile extends Component {
   }
 
   render() {
-    const { loading, name, image, email, description } = this.state;
+    const { name, image, email, description } = this.state;
     return (
       <div>
         <Header />
         <div data-testid="page-profile">
-          { loading
-            ? <OnLoad />
-            : (
-              <div>
-                <section>
-                  <img data-testid="profile-image" src={ image } alt={ name } />
-                  <Link to="/profile/edit">Editar perfil</Link>
-                  <p>
-                    Nome:
-                  </p>
-                  <p>
-                    { name }
-                  </p>
-                  <p>
-                    E-mail:
-                  </p>
-                  <p>
-                    { email }
-                  </p>
-                  <p>
-                    Descrição:
-                  </p>
-                  <p>
-                    { description }
-                  </p>
-                </section>
-              </div>
-            ) }
+          <div>
+            <section>
+              <img data-testid="profile-image" src={ image } alt={ name } />
+              <Link to="/profile/edit">Editar perfil</Link>
+              <p>Nome:</p>
+              <p>{ name }</p>
+              <p>E-mail:</p>
+              <p>{ email }</p>
+              <p>Descrição:</p>
+              <p>{ description }</p>
+            </section>
+          </div>
         </div>
       </div>
     );
